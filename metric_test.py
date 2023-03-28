3dd80efe87cedd442a4a0381256cb4d847e4fb05
Sarah Wang <55766946+sarahsonder@users.noreply.github.com>
2023-03-28 18:38:22 -0400

    Delete metric_test.py

--- metric_test.py
+++
@@ -1,3 +0,0 @@
-target = 'spotify:track:7qjF1j0sMTLcmiTKXwVh09'
-other_songs = ['spotify:track:566yEfNfBoojymRCZRbkxU', 'spotify:track:136J2Jkg8sG7P5wilO9P1R']
-scores = similarity_score(target, other_songs)

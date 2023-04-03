c04bb4942db0d1b2abe3d21699db2ee8cc5a0004
Rebecca <126980734+rrjk76@users.noreply.github.com>
2023-04-02 22:25:48 -0400

    Add files via upload

--- handle_csv.py
+++ handle_csv.py
@@ -21,78 +21,68 @@ from typing import Optional
 from track import Track
 
 
-def filter_csv(song_file: str) -> dict:
+def filter_csv(csv_file: str) -> dict[str, list[str]]:
     """Returns a dictionary based on song_file. The key is the name and the artist of the song all lowercased,
     concatenated, with all spaces removed. Then, the associated value is the track id.
 
     Preconditions:
         - song_file refers to a valid csv file
     """
-    with open(song_file) as csv_file:
+    with open(csv_file) as csv_file:
         reader = csv.reader(csv_file)
         next(reader)
 
         song_dict = {}
         for row in reader:
-            song_name = row[1].replace(' ', '')
-            song_name = song_name.replace(',', '')
-            artist_name = row[2].replace(' ', '')
-            song_dict[song_name.lower() + artist_name.lower()] = row[0]
+            song_dict[row[3]] = [row[0], row[1], row[2]]
 
     return song_dict
 
 
-def find_single_song(song_file: str, song: str) -> Optional[str]:
+def find_song(song_dict: dict[str, list[str]], user_entry: str) -> Optional[Track]:
     """Returns a Track if the song the user inputs is found in the song_file, and returns None otherwise."""
-    song_dict = filter_csv(song_file)
-    song = song.replace(' ', '')
-    song = song.replace(',', '')
-    if song in song_dict:
-        return song.lower()
+    if user_entry in song_dict:
+        track_id = song_dict[user_entry][0]
+        return Track(track_id)
     else:
         return None
 
 
-def find_song(song_dict: dict, song: str) -> Optional[Track]:
-    """Returns a Track if the song the user inputs is found in the song_file, and returns None otherwise."""
-    if song in song_dict:
-        return Track(song_dict[song])
-    else:
-        return None
+def find_target_song(song_dict: dict[str, list[str]], song: str) -> str:
+    """Returns Sptofy URI."""
+    track_id = song_dict[song][0]
+    uri = 'spotify:track:' + track_id
+    return uri
 
 
-def most_similar_songs(song_file: str, song: str, user_preferences: list[float]) -> dict:
-    """Returns a list of the top 10 most similar songs from a csv file.
+def most_similar_songs(song_dict: dict[str, list[str]], target: Track, user_preferences: list[float]) -> dict[str, str]:
+    """Returns a dictionary of the top 10 most similar songs from a csv file.
 
     Preconditions:
         - song_file refers to a valid csv file
         - song != ''
         - len(user_preferences) == 11
     """
-    song_dict = filter_csv(song_file)
-    target = find_song(song_dict, song)
     track_lst = []
     returned_lst = {}
-    track_song_and_artist = {}
 
-    if target is not None:
-        song_dict.pop(song)
-        for song in song_dict:
-            song = Track(song_dict[song])
-            track_lst.append(song)
-            track_song_and_artist[song.features['track_name']] = song.features['track_artist']
+    song_dict.pop(target.features['title_and_artist'])
 
-        similarity_score = target.calc_similarity_score(track_lst, user_preferences)
-        sorted_similarity_scores = sorted(similarity_score.items(), key=lambda item: item[1], reverse=True)
+    for song in song_dict:
+        song = Track(song_dict[song][0])
+        track_lst.append(song)
 
-        if len(sorted_similarity_scores) > 10:
-            sorted_similarity_scores = sorted_similarity_scores[:11]
+    similarity_score = target.calc_similarity_score(track_lst, user_preferences)
+    sorted_similarity_scores = sorted(similarity_score.items(), key=lambda item: item[1], reverse=True)
 
-        for song in sorted_similarity_scores:
-            if similarity_score[song[0]] >= 85:
-                similar_ids = {track.track_id for track in track_lst if track.features['track_name'] == song[0]}
+    if len(sorted_similarity_scores) > 10:
+        sorted_similarity_scores = sorted_similarity_scores[:11]
 
-                artist = {track_song_and_artist[track] for track in track_song_and_artist if song[0] == track}
-                returned_lst[similar_ids.pop()] = song[0] + ' by ' + artist.pop()
+    for song in sorted_similarity_scores:
+        if similarity_score[song[0]] >= 85:
+            similar_ids = {track.track_id for track in track_lst if track.features['track_name'] == song[0]}
+
+            artist = {song_dict[track][2] for track in song_dict if song[0] == song_dict[track][1]}
+            returned_lst[similar_ids.pop()] = song[0] + ' by ' + artist.pop()
 
     return returned_lst
